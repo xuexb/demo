7b2d273cb3092d26d9f595e96e7cbcc02e665ffb
xiaowu <admin@xuexb.com>
2015-06-02 19:18:18 +0800

    添加默认选中的单元测试

--- tests/ui-radio/tests.js
+++ tests/ui-radio/tests.js
@@ -9,8 +9,8 @@
 
     var is_ie_78 = !window.addEventListener; //针对ie78测试一些ui的显示问题，通过判断是否有相关的类来表示是否ok
 
-    test("使用prop操作", function() {
-        var $input = $('<label class="ui-radio">' +
+    asyncTest("使用prop操作", function() {
+        var $wrap = $('<label class="ui-radio">' +
             '<input type="radio" name="xieliang" value="谢亮">' +
             '<span>谢亮</span>' +
             '</label><label class="ui-radio">' +
@@ -18,33 +18,60 @@
             '<span>xuexb</span>' +
             '</label>').appendTo('#qunit-fixture');
 
-        $input = $input.find('input');
+        var $input = $wrap.find('input');
+
         //实例
         $input.radioMM();
 
 
         //选中
-        stop();
         $input.eq(0).prop('checked', true);
         strictEqual($input.get(0).checked, true, '选中');
-        start();
 
         //取消选中
         //因为在ie78里单选选中后不支持取消,只能让别的按钮选中
-        stop();
         $input.eq(1).prop('checked', true);
         strictEqual($input.get(0).checked, false, '取消选中');
-        start();
 
         if (is_ie_78) {
             $input.eq(0).prop('checked', true);
             strictEqual($input.eq(0).parent().hasClass('checked'), true, '选中的ui');
 
             //因为在ie78里单选选中后不支持取消,只能让别的按钮选中
-            stop();
             $input.eq(1).prop('checked', true);
             strictEqual($input.eq(0).parent().hasClass('checked'), false, '取消选中的ui');
-            start();
         }
+
+        QUnit.start();
+    });
+
+
+    asyncTest("默认选中", function() {
+        var $wrap = $('<label class="ui-radio">' +
+            '<input type="radio" name="xieliang" value="谢亮" checked>' +
+            '<span>谢亮</span>' +
+            '</label><label class="ui-radio">' +
+            '<input type="radio" name="xieliang" value="xuexb">' +
+            '<span>xuexb</span>' +
+            '</label>').appendTo('#qunit-fixture');
+
+        var $input = $wrap.find('input');
+
+        //实例
+        $input.radioMM();
+
+
+        //选中的值
+        strictEqual($input.filter(':checked').val(), '谢亮', '选中的值');
+
+        if (is_ie_78) {
+            stop();
+            setTimeout(function(){
+                start();
+                strictEqual($input.filter(':checked').parent().hasClass('checked'), true, '选中的ui');
+            }, 1);
+        }
+
+        QUnit.start();
     });
 })();
