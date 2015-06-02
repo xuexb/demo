3bc4f150c7cb29e26a1e7f85a4ec75daad5fdcf0
xiaowu <admin@xuexb.com>
2015-06-02 19:07:04 +0800

    添加ie78

--- tests/ui-radio/tests.js
+++ tests/ui-radio/tests.js
@@ -1,59 +1,50 @@
- /**
-  * 美化单选测试用例
-  * @author xieliang
-  * @email admin@xuexb.com
-  */
+/**
+ * 美化单选测试用例
+ * @author xieliang
+ * @email admin@xuexb.com
+ */
 
- (function() {
-     'use strict';
+(function() {
+    'use strict';
 
-     var is_ie_78 = !window.addEventListener; //针对ie78测试一些ui的显示问题，通过判断是否有相关的类来表示是否ok
+    var is_ie_78 = !window.addEventListener; //针对ie78测试一些ui的显示问题，通过判断是否有相关的类来表示是否ok
 
-     test("使用prop操作", function() {
-         var $input = $('<label class="ui-radio">' +
-             '<input type="radio" name="xieliang" value="谢亮">' +
-             '<span>谢亮</span>' +
-             '</label><label class="ui-radio">' +
-             '<input type="radio" name="xieliang" value="xuexb">' +
-             '<span>xuexb</span>' +
-             '</label>').appendTo('#qunit-fixture');
+    test("使用prop操作", function() {
+        var $input = $('<label class="ui-radio">' +
+            '<input type="radio" name="xieliang" value="谢亮">' +
+            '<span>谢亮</span>' +
+            '</label><label class="ui-radio">' +
+            '<input type="radio" name="xieliang" value="xuexb">' +
+            '<span>xuexb</span>' +
+            '</label>').appendTo('#qunit-fixture');
 
-         $input = $input.find('input');
-         //实例
-         $input.radioMM();
-
-
-         //选中
-         stop();
-         $input.eq(0).prop('checked', true);
-         strictEqual($input.get(0).checked, true, '选中');
-         start();
-
-         //取消选中
-         //因为在ie78里单选选中后不支持取消,只能让别的按钮选中
-         stop();
-         $input.eq(1).prop('checked', true);
-         console.log($input.get(0).checked);
-         console.log($input.get(1).checked);
-         strictEqual($input.get(0).checked, false, '取消选中');
-         start();
+        $input = $input.find('input');
+        //实例
+        $input.radioMM();
 
 
-         if (is_ie_78) {
-             $input.prop('checked', true);
-             strictEqual($input.parent().hasClass('checked'), true, '选中的ui');
+        //选中
+        stop();
+        $input.eq(0).prop('checked', true);
+        strictEqual($input.get(0).checked, true, '选中');
+        start();
 
-             stop();
+        //取消选中
+        //因为在ie78里单选选中后不支持取消,只能让别的按钮选中
+        stop();
+        $input.eq(1).prop('checked', true);
+        strictEqual($input.get(0).checked, false, '取消选中');
+        start();
 
-             setTimeout(function() {
-                 $input.prop('checked', false);
-                 console.log($input.parent().hasClass('checked'));
-                 console.log($input.parent().attr('class'));
-                 strictEqual($input.parent().hasClass('checked'), false, '取消选中的ui');
-                 start();
-             }, 1000);
+        if (is_ie_78) {
+            $input.eq(0).prop('checked', true);
+            strictEqual($input.eq(0).parent().hasClass('checked'), true, '选中的ui');
 
-
-         }
-     });
- })();
+            //因为在ie78里单选选中后不支持取消,只能让别的按钮选中
+            stop();
+            $input.eq(1).prop('checked', true);
+            strictEqual($input.eq(0).parent().hasClass('checked'), false, '取消选中的ui');
+            start();
+        }
+    });
+})();
