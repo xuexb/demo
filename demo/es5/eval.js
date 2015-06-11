4540ffba5b2e50e5f3933583a067a1f58e4ea3f4
xiaowu <admin@xuexb.com>
2015-06-11 08:32:22 +0800

    修改eval

--- demo/es5/eval.js
+++ demo/es5/eval.js
@@ -1,7 +1,30 @@
 'use strict';
 
 function demo1(){
-    var arr = [1, '10', 2, 30]
+    var arr = [1, '10', 2, 30];
+    console.log(eval(arr.join('+')));
 }
-
 demo1();
+
+
+this.xx = 1;
+function demo2(){
+    eval("("+ (this.xx=2) +")");
+}
+demo2();
+console.log(this.xx);
+
+
+this.xx = 1;
+function demo3(){
+    eval("("+ (this.xx=2) +")");
+}
+demo3.call({});
+console.log(this.xx);
+
+
+function demo4(){
+    eval("(var b = 3)");
+    console.log(b);
+}
+demo4();
