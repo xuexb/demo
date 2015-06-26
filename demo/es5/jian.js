89b2283dc8156ee754d7149bda3b75132b43058f
xiaowu <admin@xuexb.com>
2015-06-26 08:52:53 +0800

    添加减法

---
+++ demo/es5/jian.js
@@ -0,0 +1,15 @@
+var jia = function(){
+    var args = [].slice.call(arguments).map(function(val){
+        return parseInt(val * 100, 10) || 0;
+    });
+
+    if(!args.length){
+        return 0;
+    }
+
+    return new Function('return ('+ args.join('-') +') / 100')();
+}
+
+console.log(jia('1',2))
+console.log(jia(.01,2))
+console.log(jia(.1,.2))
