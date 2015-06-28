e2dc743e667230b472ea142cc0a7bfe508db3e6c
xiaowu <admin@xuexb.com>
2015-06-28 12:06:26 +0800

    优化弹出层

--- demo/dialog/mod/dialog.js
+++ demo/dialog/mod/dialog.js
@@ -2,6 +2,9 @@
  * 弹出导
  * @author xiaowu
  * @email fe.xiaowu@gmail.com
+ *
+ * @description 内部方法以 __ 开头，标识类以 _ 开头
+ * @description ie7+
  */
 
 (function() {
@@ -10,20 +13,24 @@
     var $window = $(window),
         $document = $(document),
         Prefix = 'ui-dl-',
-        is_animate = !!window.FormData;
+        is_animate = !!window.addEventListener,
+        guid = 0;
 
     function Dialog(config, ok, cancel) {
         var self = this;
 
+        // 如果第一个参数为str
         if ('string' === typeof config) {
             config = {
                 content: config
             }
         }
 
+        // 合并默认配置
         config = $.extend(true, {}, Dialog.defaults, config || {});
 
-        if (!$.isArray(config.button)) { //如果参数的button不是数组则让其为数组，因为后面要进行push操作追加
+        //如果参数的button不是数组则让其为数组，因为后面要进行push操作追加
+        if (!$.isArray(config.button)) {
             config.button = [];
         }
 
@@ -33,8 +40,8 @@
             config.ok = ok;
         }
 
-        console.log(config.ok)
-        if (config.ok) { //如果有确认按钮则追加到button数组里
+        // 如果有确认按钮则追加到button数组里
+        if (config.ok) {
             config.button.push({
                 id: 'ok',
                 value: config.okValue,
@@ -50,7 +57,8 @@
             config.cancel = cancel;
         }
 
-        if (config.cancel) { //如果有取消按钮则追加到button数组里
+        //如果有取消按钮则追加到button数组里
+        if (config.cancel) {
             config.button.push({
                 id: 'cancel',
                 value: config.cancelValue,
@@ -59,8 +67,9 @@
         }
 
         // 更新 zIndex 全局配置
-        Dialog.defaults.zIndex = config.zIndex; //把参数里的zindex更新到全局对象里
+        Dialog.defaults.zIndex = config.zIndex;
 
+        // 写入配置
         self.__config = config;
 
         /**
@@ -69,82 +78,95 @@
          */
         self.__jquery = {};
 
+        /**
+         * 事件空间
+         * @type {Object}
+         */
         self.__listeners = {};
 
+        // 创建骨架到页面，但是看不到类
         self.__create();
 
-        console.log(config)
-        self.button.apply(self, config.button); //处理按钮组
 
-        self.title(config.title);
-        self.width(config.width, true);
-        self.height(config.height, true);
-        self.position();
+        self.button.apply(self, config.button) //设置按钮
+            .time(config.time) //设置时间
+            .title(config.title) //设置标题
+            .width(config.width, true) //设置宽
+            .height(config.height, true) //设置高
+            .position(); //定位
 
+        // 如果支持动画则添加css3类让其动画显示出来
         if (is_animate) {
             self.on('show', function() {
-                setTimeout(function() {
-                    self.find('wrap').addClass(Prefix + 'show');
-                });
-            });
-            self.on('hide', function() {
+                //强制重排
+                self.find('wrap').get(0).offsetWidth;
+                self.find('wrap').addClass(Prefix + 'show');
+            }).on('hide', function() {
                 this.find('wrap').removeClass(Prefix + 'show');
             });
         }
 
+        // 绑定关闭事件
         self.find('close').on('click', function() {
-            self.close();
+            return !self.close();
         });
 
+        // 绑定点击层的时候置顶
         self.find('wrap').on('mousedown', function() {
             self.zIndex();
         });
 
+        // 绑定按钮组事件，禁用和loading不可点
         self.find('buttons').on('click', 'a', function() {
             var that = this,
                 id, fn;
 
-            if (that.className.indexOf('disabled') > -1) {
+            if (that.className.indexOf('disabled') > -1 || that.className.indexOf('loading') > -1) {
                 return false;
             }
 
-            id = $(that).data(Prefix + 'callback');
+            id = $(that).data('id');
             fn = self.__listeners[id];
 
-            if (fn && 'function' === typeof fn.callback && fn.callback.call(self) !== false) {
-                self.close();
+            if (fn && 'function' === typeof fn.callback && fn.callback.call(self) === false) {
+                return false;
             }
 
-            return false;
+            return !self.close();
         });
 
-        setTimeout(function() {
-            self[config.show ? 'show' : 'hide']();
+        // 显示/隐藏
+        self[config.visible ? 'show' : 'hide']();
 
-            if (config.lock) {
-                self.lock();
-            }
+        // 如果锁屏
+        if (config.lock) {
+            self.lock();
+        }
+
+        //设置全局id
+        self._guid = ++guid;
+
+        // 绑定resize定位，这里不用cache是因为jquery触发resize本来就是队列形式，所有没有全局cache
+        $window.on('resize.dialog-' + guid, function() {
+            self.position();
         });
     }
 
+    /**
+     * 置顶
+     * @return self
+     */
     Dialog.prototype.zIndex = function() {
         var self = this,
             zIndex = Dialog.defaults.zIndex + 2;
 
         self.find('wrap').css('zIndex', zIndex);
 
-        if (Dialog.focus) {
-            Dialog.focus.find('wrap').removeClass(Prefix + 'focus');
-        }
-
-        Dialog.focus = self;
-
-        self.find('wrap').addClass(Prefix + 'focus');
-
-        if (self.locked) {
+        if (self.__jquery.mask) {
             self.find('mask').css('zIndex', zIndex - 1);
         }
 
+        // 重置到默认，让以后弹的层都在其之上
         Dialog.defaults.zIndex = zIndex;
 
         return self;
@@ -153,37 +175,38 @@
     /**
      * 绑定事件
      * @param {string} name 事件名，支持login,exit
-     * @param {string} callback 事件回调
+     * @param {function|boolean} callback 事件回调
      * @return self
      */
     Dialog.prototype.on = function(name, callback) {
-        if ('function' === typeof callback) {
-            this.__getListener(name).push({
-                callback: callback
-            });
-        }
+        this.__getListener(name).push({
+            callback: callback === true ? function() {
+                return true;
+            } : callback === false ? function() {
+                return false;
+            } : 'function' === typeof callback ? callback : function() {}
+        });
         return this;
     }
 
     /**
      * 绑定事件（只触发单次）
      * @param {string} name 事件名，支持login,exit
-     * @param {string} callback 事件回调
+     * @param {function|boolean} callback 事件回调
      * @return self
      */
     Dialog.prototype.one = function(name, callback) {
         // 内部方法，为了卸载
-        var __call = function() {
+        var fn;
+        fn = callback === true ? true : callback === false ? false : 'function' === typeof callback ? function() {
             //卸载
-            this.off(name, __call);
+            this.off(name, fn);
 
             // 执行回调
-            if ('function' === typeof callback) {
-                callback.apply(this, [].slice.call(arguments));
-            }
-        }
+            callback.apply(this, [].slice.call(arguments));
+        } : null;
 
-        return this.on(name, __call);
+        return this.on(name, fn);
     }
 
     /**
@@ -261,7 +284,7 @@
      * 按钮
      * @example
      *     ({id:'',name:'',callback:noop},{})
-     * @return {[type]} [description]
+     * @return self
      */
     Dialog.prototype.button = function() {
         var self = this,
@@ -292,7 +315,8 @@
             }
 
             //写入到按钮和空间上
-            button.innerHTML = callback[id].value = value;
+            callback[id].value = value;
+            button.innerHTML = '<span>' + value + '</span>';
 
 
             //如果禁用
@@ -300,21 +324,25 @@
                 className += ' disabled'; //禁用的class
             } else {
                 //如果有回调
-                if (val.callback) {
+                if (val.callback !== undefined) {
                     callback[id].callback = val.callback;
                 }
             }
 
             if (val.focus) { //如果为聚焦
                 if (self._focus) {
-                    self._focus.removeClass(Prefix + 'button-focus'); //移除老聚焦的按钮
+                    self._focus.removeClass('focus'); //移除老聚焦的按钮
                 }
-                className += ' ' + Prefix + 'button-focus'; //给当前添加聚焦
+                className += ' focus'; //给当前添加聚焦
                 self._focus = $(button);
             }
 
             if (val.light) { //如果高亮
                 className += ' light';
+            }
+
+            if (val.loading) {
+                className += ' loading';
             }
 
             if (val.className) { //如果配置的按钮有class则追加下
@@ -326,7 +354,7 @@
             button.className = className;
 
 
-            button.setAttribute('data-' + Prefix + 'callback', id); //为了委托事件用
+            button.setAttribute('data-id', id); //为了委托事件用
 
             if (isNewButton) { //如果为新按钮则追加到dom
                 callback[id].elem = button;
@@ -340,55 +368,74 @@
             self._focus.focus(); //只操作按钮的焦点，而不管窗口的焦点，否则ie6有严重bug
         }
         button = null;
-        return self;
+        return self.position();
     }
 
+    /**
+     * 显示
+     * @return self
+     */
     Dialog.prototype.show = function() {
         var self = this;
 
-        if (self.visibled) {
+        if (self._visibled) {
             return self;
         }
 
-        self.visibled = true;
+        self._visibled = true;
 
         if (self.__jquery.mask) {
             self.find('mask').show();
         }
 
         self.find('wrap').show();
+
+        // 触发显示回调
         self.__trigger('show');
 
         return self.zIndex();
     }
+
+    /**
+     * 隐藏
+     * @return self
+     */
     Dialog.prototype.hide = function() {
         var self = this;
 
-        if (self.visibled === false) {
+        if (self._visibled === false) {
             return self;
         }
 
-        self.visibled = false;
+        self._visibled = false;
 
         if (self.__jquery.mask) {
             self.find('mask').hide();
         }
 
         self.find('wrap').hide();
+
+        // 触发隐藏回调
         self.__trigger('hide');
 
         return self;
     }
 
+    /**
+     * 关闭层
+     * @description 与hide的区别是close后就会销毁，不能再显示
+     * @return self
+     */
     Dialog.prototype.close = function() {
         var self = this;
-        if (this.__trigger('close') === false) {
+
+        // 如果已经关闭或者回调里返回false则不关闭
+        if (self._closed || this.__trigger('close') === false) {
             return self;
         }
 
-        if (Dialog.focus === self) {
-            Dialog.focus = null;
-        }
+        // 卸载事件
+        $window.off('resize.dialog-' + self._guid);
 
         if (self.__jquery.mask) {
             self.find('mask').remove();
@@ -400,19 +447,25 @@
             delete self[key];
         }
 
+        self._closed = true;
+
         return self;
     }
 
+    /**
+     * 关闭锁屏
+     * @return self
+     */
     Dialog.prototype.unlock = function() {
         var self = this;
-        if (!self.locked) {
+
+        if (!self._locked) {
             return self;
         }
 
         self.find('mask').remove();
 
-        self.find('wrap').removeClass(Prefix + 'lock');
-        self.locked = false;
+        self._locked = false;
 
         //删除元素引用
         delete self.__jquery.mask;
@@ -420,12 +473,16 @@
         return self;
     }
 
+    /**
+     * 锁屏
+     * @return self
+     */
     Dialog.prototype.lock = function() {
         var self = this,
             config = self.__config,
             div, css;
 
-        if (self.locked) {
+        if (self._locked) {
             return self;
         }
 
@@ -447,20 +504,23 @@
         document.body.appendChild(div);
 
 
-        if (self.visibled) {
+        if (self._visibled) {
             css.display = 'block';
         }
 
         self.__jquery.mask = $(div).css(css);
-        self.locked = true;
+        self._locked = true;
 
-
+        div = null;
 
         self.zIndex().find('wrap').addClass(Prefix + 'lock');
         return self;
     }
 
-
+    /**
+     * 定位
+     * @return self
+     */
     Dialog.prototype.position = function() {
         var wrap = this.find('wrap')[0],
             fixed = this.__config.fixed, //判断是否为fixed定位
@@ -480,16 +540,33 @@
 
     }
 
+    /**
+     * 设置宽
+     * @param  {string|int} val  宽
+     * @param  {boolean} mark 如果有值则不触发定位
+     * @return self
+     */
     Dialog.prototype.width = function(val, mark) {
         this.find('content').width(val);
         return mark ? this : this.position();
     }
 
+    /**
+     * 设置高
+     * @param  {string|int} val  高
+     * @param  {boolean} mark 如果有值则不触发定位
+     * @return self
+     */
     Dialog.prototype.height = function(val, mark) {
         this.find('content').height(val);
         return mark ? this : this.position();
     }
 
+    /**
+     * 设置标题
+     * @param  {boolean|string} title 设置标题，如果为false则不显示标题
+     * @return self
+     */
     Dialog.prototype.title = function(title) {
         if (title === false) {
             this.find('wrap').addClass(Prefix + 'no-title');
@@ -500,6 +577,11 @@
         return this;
     }
 
+    /**
+     * 查找弹层里面jquery元素
+     * @param  {string} name 元素名，支持 wrap,foot,buttons,header,title,close,inner,content
+     * @return jquery元素
+     */
     Dialog.prototype.find = function(name) {
         var $elem = this.__jquery[name];
 
@@ -510,25 +592,51 @@
         return $elem;
     }
 
-    Dialog.prototype.__create = function() {
-        var tpl, wrap;
+    /**
+     * 设置时间
+     * @param  {undefined|int} num 关闭时间单位ms，为空则清除自动关闭
+     * @return self
+     */
+    Dialog.prototype.time = function(num) {
+        var self = this,
+            timer = self._timer;
 
-        tpl = '<div class="ui-dl-inner">\
+        if (timer) {
+            clearTimeout(timer);
+        }
+
+        if (num) {
+            self._timer = setTimeout(function() {
+                self.close();
+            }, num);
+        }
+
+
+        return self;
+    }
+
+    /**
+     * 内部创建弹层到页面
+     */
+    Dialog.prototype.__create = function() {
+        var config, tpl, wrap;
+
+        config = this.__config;
+
+        tpl = '<div class="ui-dl-inner" i="inner">\
                 <div class="ui-dl-hd" i="header">\
-                    <h3 i="title">我是标题</h3>\
+                    <h3 i="title"></h3>\
                     <a class="ui-dl-close" i="close" href="#"><i>关闭</i></a>\
                 </div>\
-                <div class="ui-dl-cnt" i="content">' + this.__config.content + '</div>\
+                <div class="ui-dl-cnt" i="content" style="padding:' + config.padding + '">' + config.content + '</div>\
                 <div class="ui-dl-ft" i="foot">\
-                    <div class="ui-dl-clear">\
-                        <div class="ui-dl-btn" i="buttons"></div>\
-                    </div>\
+                    <div class="ui-dl-btn" i="buttons"></div>\
                 </div>\
             </div>';
 
         wrap = document.createElement('div');
         wrap.innerHTML = tpl;
-        wrap.style.cssText = 'position:absolute;top:-9999rem;';
+        wrap.style.cssText = 'position:' + (config.fixed ? 'fixed' : 'absolute') + ';top:-9999rem;';
 
         wrap.className = this.__config.className;
 
@@ -539,26 +647,27 @@
         wrap = null;
     }
 
-    Dialog.focus = null;
-
     /**
      * 默认配置参数
      * @type {Object}
      */
     Dialog.defaults = {
-        content: 'loading...',
-        title: '标题',
-        lock: true,
-        className: '',
-        width: 'auto',
-        height: 'auto',
-        show: true,
-        zIndex: 1990,
-        ok: function() {},
-        cancel: function() {},
-        button: [],
-        okValue: '确定',
-        cancelValue: '取消'
+        padding: '20px 30px', //内容填充
+        content: 'loading...', //内部
+        title: '标题', //标题
+        fixed: false, //是否fixed
+        lock: true, //是否遮罩
+        className: '', //类名
+        width: 'auto', //宽
+        height: 'auto', //高
+        visible: true, //是否可见
+        zIndex: 1990, //默认索引
+        ok: null, //确定按钮
+        cancel: null, //取消按钮
+        button: [], //按钮组
+        okValue: '确定', //确定
+        cancelValue: '取消', //取消
+        time: null //自动关闭
     }
 
     window.Dialog = Dialog;
