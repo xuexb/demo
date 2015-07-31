fd7471e5b9408a92e22fef5a36e8846262060e49
xiaowu <fe.xiaowu@gmail.com>
2015-07-31 23:59:41 +0800

    添加缓存

---
+++ demo/haosou_home_cache/cache.js
@@ -0,0 +1,102 @@
+/**
+ * @file 360缓存模拟
+ * @author xiaowu
+ * @email fe.xiaowu@baidu.com
+ */
+
+(function (window, document) {
+    'use strict';
+
+    /**
+     * 暴露空间
+     * @type {Object}
+     */
+    var cache = window.cache = {};
+
+    /**
+     * 别名
+     * @type {Object}
+     */
+    var cname = {
+        js: 'script',
+        css: 'style'
+
+    };
+
+    /**
+     * 写入cookie
+     *
+     * @param  {string} key   名
+     * @param  {string} value 值
+     */
+    var setcookie = function (key, value) {
+        var expires;
+        var date = new Date(new Date().getTime() + 24 * 60 * 60 * 1000);
+        expires = '; expires=' + date;
+        document.cookie = key + '=' + encodeURIComponent(value) + ';domain=' + document.domain + expires;
+    };
+
+    /**
+     * 使用数据插入到页面head中
+     *
+     * @param  {string} tagName 标签名
+     * @param  {string} data    数据
+     */
+    var append = function (tagName, data) {
+        var div = document.createElement(tagName);
+        div.innerHTML = data;
+        document.head.appendChild(div);
+        div = null;
+    };
+
+    /**
+     * 读取缓存并写入到页面中
+     *
+     * @param  {string} id   缓存id
+     * @param  {string} type 缓存类型，有js,css
+     */
+    var cache2html = function (id, type) {
+        var data = localStorage[id];
+
+        if (!data) {
+            return;
+        }
+
+        append(cname[type], data);
+    };
+
+    /**
+     * 写入缓存
+     *
+     * @param  {string} id 元素id
+     */
+    var html2cache = function (id) {
+        var elem = document.getElementById(id);
+
+        if (!elem) {
+            return;
+        }
+
+        setcookie(id, '1');
+
+        localStorage[id] = elem.innerHTML.trim();
+    };
+
+    /**
+     * 空方法
+     */
+    var noop = function () {};
+
+    // 如果支持缓存
+    if (window.localStorage) {
+        cache.get = cache2html;
+        cache.set = html2cache;
+    }
+    else {
+        cache = {
+            set: noop,
+            get: noop
+
+        };
+    }
+})(window, document);
