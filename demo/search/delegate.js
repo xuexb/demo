580be44f2aeef5df9d517884d460bd3db89b17a6
xiaowu <fe.xiaowu@gmail.com>
2015-08-16 19:53:03 +0800

    test-ssh

--- demo/search/delegate.js
+++ demo/search/delegate.js
@@ -1,4 +1,4 @@
-(function(){
+(function () {
     'use strict';
 
     /**
@@ -14,30 +14,33 @@
     /**
      * 负责发送请求
      */
-    var send = function(){
+    var send = function () {
         $.ajax({
             url: ajaxUrl,
             data: data,
             type: 'GET',
             dataType: 'json',
-            success: function(res){
+            success: function (res) {
                 $wirte.text(JSON.stringify(res));
             },
-            error: function(){
+            error: function () {
                 $wirte.text('出错');
             }
-        });
-    }
 
-    $('#J-demo').on('click', 'li', function(){
+        });
+    };
+
+    $('#J-demo').on('click', 'li', function () {
         var id = $(this).data('id');
         var type = $(this).data('type');
 
         $(this).addClass('current').siblings().removeClass('current');
 
-        if(!id){
-            delete data[type];
-        } else {
+        if (!id) {
+            delete data[type]
+            ;
+        }
+        else {
             data[type] = id;
         }
 
