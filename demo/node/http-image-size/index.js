d5d31f6c67c0694482c5ce0455a3bad84e7a5be4
xuexb <fe.xiaowu@gmail.com>
2015-11-22 18:28:27 +0800

    添加获取图片

--- demo/node/http-image-size/index.js
+++ demo/node/http-image-size/index.js
@@ -1,5 +1,7 @@
 var size = require('http-image-size');
 
-size('https://github.xuexb.com/uploads/docjs/1.png', function (err, dimensions, length) {
+console.time('a');
+size('https://github.xuexb.com/uploads/docjs/1.spng', function (err, dimensions, length) {
     console.log(err, dimensions, length);
+    console.timeEnd('a')
 });
