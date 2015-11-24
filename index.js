4188282125d63f532ff910f30bc6c71c3411c39b
xuexb <fe.xiaowu@gmail.com>
2015-11-24 17:48:28 +0800

    修改hook

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 'use strict';
 
 var Mdocjs = require('mdjs');
+var child_process = require('child_process');
 
 var app = new Mdocjs().clear_cache();
 
 // 更新勾子
 app.express.post('/update', function (req, res, next) {
-    require('child_process').exec('git pull', {
+    child_process.exec('git pull', {
         cwd: app.options.root
     }, function (a, b) {
         console.log(a, b, new Date().getTime());
 
         // 清空缓存
         app.clear_cache();
+
+        // 重启pm2
+        child_process.exec('pm2 restart index.js', {
+            cwd: app.options.root
+        });
     });
 
     res.end('ok');
