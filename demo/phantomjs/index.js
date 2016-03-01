107a9ec56f8bc05cf44a1937050d1683acdb5393
xuexb <fe.xiaowu@gmail.com>
2016-03-01 11:08:09 +0800

    add: 添加phantomjs例子

---
+++ demo/phantomjs/index.js
@@ -0,0 +1,33 @@
+var page = require('webpage').create();
+// page.onResourceRequested = function (request) {
+//     console.log('Request ' + JSON.stringify(request, undefined, 4));
+// };
+// page.onResourceReceived = function (response) {
+//     console.log('Receive ' + JSON.stringify(response, undefined, 4));
+// };
+
+page.viewportSize = { width: 1280, height: 800 };
+
+
+page.open('http://www.meishichina.com', function () {
+    var title = page.evaluate(function () {
+        return document.title;
+    });
+    var data = page.evaluate(function () {
+        var $elem = $('.nav_wrap');
+
+        return {
+            width: $elem.width(),
+            height: $elem.height(),
+            left: $elem.offset().left,
+            top: $elem.offset().top
+        };
+    });
+
+    console.log(data.width, data.height, data.left, data.top);
+
+    page.clipRect = data;
+
+    page.render('meishi.png');
+    phantom.exit();
+});
