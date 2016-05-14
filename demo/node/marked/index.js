27d11a3ca1a7520039025d20487c874527b4f10b
xuexb <fe.xiaowu@gmail.com>
2016-05-15 00:13:10 +0800

    update

--- demo/node/marked/index.js
+++ demo/node/marked/index.js
@@ -1,52 +1,6 @@
-// node
-'use strict';
-
-var fs = require('fs');
-var tpl = fs.readFileSync('./tpl.md').toString();
 var marked = require('marked');
 
-var guid = 1;
-var arr = [];
-var obj = {};
+var str = '[window.close()](javascript:window.close())';
 
-var renderer = new marked.Renderer();
-
-// 渲染代码
-renderer.code = function (data, lang) {
-    var id = guid ++;
-
-    arr.push(id);
-
-    data = require('highlight.js').highlightAuto(data).value;
-    obj[id] = '<pre><code class="hljs lang-' + lang + '">' + data + '</code></pre>';
-console.log('{{{'+ id +'}}}')
-    return '{{{'+ id +'}}}';    
-};
-
-// 配置
-marked.setOptions({
-    renderer: renderer,
-    gfm: true,
-    tables: true,
-    breaks: false,
-    pedantic: false,
-    sanitize: true,
-    smartLists: true,
-    smartypants: false
-
-});
-
-// 转换
-tpl = marked(tpl);
-
-// 去换行
-tpl = tpl.replace(/[\r\n]/g, '');
-
-arr.forEach(function(id){
-    tpl = tpl.replace('{{{'+ id +'}}}', obj[id] || '');
-    console.log(id)
-});
-
-console.log(arr)
-
-// console.log(tpl);
+console.log(str);
+console.log(marked(str));
